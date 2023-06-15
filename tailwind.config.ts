a0dbb7bab4a9229b3a58fc0e3f02ab3866117002
Stefan Probst <stefanprobst@posteo.de>
2023-06-15 20:38:51 +0200

    fix: fix typo in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const primary = colors.slate;
 
 const config: Config = {
 	content: [
-		"./src/components**/*.@(css|ts|vue)",
+		"./src/components/**/*.@(css|ts|vue)",
 		"./src/content/**/*.md",
 		"./src/layouts/**/*.@(css|ts|vue)",
 		"./src/pages/**/*.@(css|ts|vue)",
