de0a179d85571a4516b4109f0ce306eff7ede784
Stefan Probst <stefanprobst@posteo.de>
2023-06-16 13:56:45 +0200

    test: run a11y tests with axe

--- e2e/pages/imprint.test.ts
+++ e2e/pages/imprint.test.ts
@@ -1,17 +1,28 @@
+import AxeBuilder from "@axe-core/playwright";
 import { expect, test } from "@playwright/test";
 
-test("imprint page should have document title", async ({ page }) => {
-	await page.goto("/en/imprint");
-	await expect(page).toHaveTitle("Imprint | ACDH-CH App");
+test.describe("imprint page", () => {
+	test("should have document title", async ({ page }) => {
+		await page.goto("/en/imprint");
+		await expect(page).toHaveTitle("Imprint | ACDH-CH App");
 
-	await page.goto("/de/imprint");
-	await expect(page).toHaveTitle("Impressum | ACDH-CH App");
-});
+		await page.goto("/de/imprint");
+		await expect(page).toHaveTitle("Impressum | ACDH-CH App");
+	});
 
-test("imprint page should have imprint text", async ({ page }) => {
-	await page.goto("/en/imprint");
-	await expect(page.getByRole("main")).toContainText("Legal disclosure");
+	test("should have imprint text", async ({ page }) => {
+		await page.goto("/en/imprint");
+		await expect(page.getByRole("main")).toContainText("Legal disclosure");
 
-	await page.goto("/de/imprint");
-	await expect(page.getByRole("main")).toContainText("Offenlegung");
+		await page.goto("/de/imprint");
+		await expect(page.getByRole("main")).toContainText("Offenlegung");
+	});
+
+	test("should not have any automatically detectable accessibility issues", async ({ page }) => {
+		await page.goto("/en");
+		expect((await new AxeBuilder({ page }).analyze()).violations).toEqual([]);
+
+		await page.goto("/de");
+		expect((await new AxeBuilder({ page }).analyze()).violations).toEqual([]);
+	});
 });
