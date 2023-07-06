75127f541e0e5e8ce0369684730d50fa0e5a7670
Stefan Probst <stefanprobst@posteo.de>
2023-07-06 13:33:19 +0200

    chore: bump deps

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,11 @@ import { defineVitestConfig } from "nuxt-vitest/config";
 export default defineVitestConfig({
 	test: {
 		environment: "nuxt",
-		globals: true,
+		environmentOptions: {
+			nuxt: {
+				domEnvironment: "jsdom",
+			},
+		},
 		include: ["./tests/*.spec.ts"],
 		setupFiles: ["../tests/setup-files/create-i18n.ts"],
 	},
