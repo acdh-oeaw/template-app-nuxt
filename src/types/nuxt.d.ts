7f56303cc558ffc289adb01ccead5868c3238ee7
Stefan Probst <stefanprobst@posteo.de>
2023-07-15 09:30:16 +0200

    fix: change import to new config folder location

--- src/types/nuxt.d.ts
+++ src/types/nuxt.d.ts
@@ -1,6 +1,6 @@
 import { type ResourcePath } from "@intlify/core-base";
 
-import { type Messages } from "~/config/i18n.config";
+import { type Messages } from "@/config/i18n.config";
 
 declare module "#app" {
 	interface PageMeta {
