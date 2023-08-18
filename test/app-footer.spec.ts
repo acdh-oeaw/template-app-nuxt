43bf68760a1b777ceea7eb355d77f3b583968713
Stefan Probst <stefanprobst@posteo.de>
2023-08-18 11:01:31 +0200

    test: use nuxt-vitest in tests

--- test/app-footer.spec.ts
+++ test/app-footer.spec.ts
@@ -1,11 +1,11 @@
-import { mount } from "@vue/test-utils";
+import { mountSuspended } from "nuxt-vitest/utils";
 import { describe, expect, it } from "vitest";
 
 import AppFooter from "@/components/app-footer.vue";
 
 describe("AppFooter", () => {
-	it("is a Vue instance", () => {
-		const wrapper = mount(AppFooter);
+	it("is a Vue instance", async () => {
+		const wrapper = await mountSuspended(AppFooter);
 		expect(wrapper.vm).toBeTruthy();
 	});
 });
--- test/app-header.spec.ts
+++ test/app-header.spec.ts
@@ -1,11 +1,11 @@
-import { mount } from "@vue/test-utils";
+import { mountSuspended } from "nuxt-vitest/utils";
 import { describe, expect, it } from "vitest";
 
 import AppHeader from "@/components/app-header.vue";
 
 describe("AppHeader", () => {
-	it("is a Vue instance", () => {
-		const wrapper = mount(AppHeader);
+	it("is a Vue instance", async () => {
+		const wrapper = await mountSuspended(AppHeader);
 		expect(wrapper.vm).toBeTruthy();
 	});
 });
