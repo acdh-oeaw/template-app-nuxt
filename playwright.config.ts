a59261b75b70f3389f4177283ed5ecaa734cb987
Stefan Probst <stefanprobst@posteo.de>
2024-01-26 11:03:30 +0100

    test: run global setup project [skip ci]

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,32 +26,45 @@ export default defineConfig({
 	},
 	projects: [
 		{
+			name: "setup",
+			testMatch: "global.setup.ts",
+		},
+		{
 			name: "chromium",
 			use: { ...devices["Desktop Chrome"] },
+			dependencies: ["setup"],
 		},
 		{
 			name: "firefox",
 			use: { ...devices["Desktop Firefox"] },
+			dependencies: ["setup"],
 		},
 		{
 			name: "webkit",
 			use: { ...devices["Desktop Safari"] },
+			dependencies: ["setup"],
 		},
+		/** Test against mobile viewports. */
 		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
+		//      name: "Mobile Chrome",
+		//      use: { ...devices["Pixel 5"] },
+		//      dependencies: ["setup"],
 		// },
 		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
+		//      name: "Mobile Safari",
+		//      use: { ...devices["iPhone 12"] },
+		//      dependencies: ["setup"],
+		// },
+		/** Test against branded browsers. */
+		// {
+		//      name: "Microsoft Edge",
+		//      use: { ...devices["Desktop Edge"], channel: "msedge" },
+		//      dependencies: ["setup"],
 		// },
 		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
+		//      name: "Google Chrome",
+		//      use: { ...devices["Desktop Chrome"], channel: "chrome" },
+		//      dependencies: ["setup"],
 		// },
 	],
 	webServer: {
