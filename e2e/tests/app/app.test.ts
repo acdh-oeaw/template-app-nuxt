699769edde436665dcbff3ff4540415a7aea03ce
Stefan Probst <stefanprobst@posteo.de>
2024-03-12 17:21:12 +0100

    test: update e2e test snapshots

--- e2e/tests/app/app.test.ts
+++ e2e/tests/app/app.test.ts
@@ -74,7 +74,7 @@ test.describe("app", () => {
 				theme_color: "#fff",
 				icons: [
 					{ src: "/icon.svg", sizes: "any", type: "image/svg+xml" },
-					// { src: "/icon-maskable.svg", sizes: "any", type: "image/svg+xml", purpose: "maskable" },
+					{ src: "/icon-maskable.svg", sizes: "any", type: "image/svg+xml", purpose: "maskable" },
 					{ src: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
 					{ src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
 				],
