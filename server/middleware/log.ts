b24f0d753182002a1426504a1b0110e39ebcfd08
Stefan Probst <stefanprobst@posteo.de>
2024-06-27 08:15:49 +0200

    fix: fix server middleware types and logging

--- server/middleware/log.ts
+++ server/middleware/log.ts
@@ -3,7 +3,7 @@ import pino from "pino-http";
 const logger = pino();
 
 export default defineEventHandler((event) => {
-	if (import.meta.env.PROD) {
+	if (process.env.NODE_ENV === "production") {
 		logger(event.node.req, event.node.res);
 	}
 });
