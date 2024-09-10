fed2fd119026b5227ddf62dbe576bcdcbf7b0ea2
Stefan Probst <stefanprobst@posteo.de>
2024-09-10 08:28:27 +0200

    chore: update deps

--- app/types/i18n.d.ts
+++ app/types/i18n.d.ts
@@ -1,4 +1,10 @@
-import type { Messages } from "@/config/i18n.config";
+import type { Locale, Messages } from "@/config/i18n.config";
+
+declare module "@intlify/core" {
+	interface IntlifyGeneratedTypeConfig {
+		locale: Locale;
+	}
+}
 
 declare module "vue-i18n" {
 	export interface DefineLocaleMessage extends Messages {}
