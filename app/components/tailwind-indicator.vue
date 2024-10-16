ed8f756dfad3996b97b713e18732d0e9d72caeff
Stefan Probst <stefanprobst@posteo.de>
2024-10-16 08:27:54 +0200

    fix: use correct class attribute in tailwind breakpoint indicator

--- app/components/tailwind-indicator.vue
+++ app/components/tailwind-indicator.vue
@@ -5,15 +5,15 @@ const isVisible = import.meta.dev;
 <template>
 	<div
 		v-if="isVisible"
-		className="fixed bottom-1 left-1 z-10 grid size-8 place-content-center rounded-full bg-on-background p-2 font-mono text-xs font-medium text-background"
+		class="fixed bottom-1 left-1 z-10 grid size-8 place-content-center rounded-full bg-on-background p-2 font-mono text-xs font-medium text-background"
 	>
-		<span className="xs:hidden">2xs</span>
-		<span className="max-xs:hidden sm:hidden">xs</span>
-		<span className="max-sm:hidden md:hidden">sm</span>
-		<span className="max-md:hidden lg:hidden">md</span>
-		<span className="max-lg:hidden xl:hidden">lg</span>
-		<span className="max-xl:hidden 2xl:hidden">xl</span>
-		<span className="max-2xl:hidden 3xl:hidden">2xl</span>
-		<span className="max-3xl:hidden">3xl</span>
+		<span class="xs:hidden">2xs</span>
+		<span class="max-xs:hidden sm:hidden">xs</span>
+		<span class="max-sm:hidden md:hidden">sm</span>
+		<span class="max-md:hidden lg:hidden">md</span>
+		<span class="max-lg:hidden xl:hidden">lg</span>
+		<span class="max-xl:hidden 2xl:hidden">xl</span>
+		<span class="max-2xl:hidden 3xl:hidden">2xl</span>
+		<span class="max-3xl:hidden">3xl</span>
 	</div>
 </template>
