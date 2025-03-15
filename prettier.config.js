98cd4626e1c13f5f9c9f05e33ae708f609a4f105
Stefan Probst <stefanprobst@posteo.de>
2025-03-15 18:39:46 +0100

    chore: improve prettier config types [skip ci]

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,22 @@
 /** @typedef {import("prettier").Config} Config */
+/** @typedef {import("prettier").Plugin} Plugin */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} */
 
 import preset from "@acdh-oeaw/prettier-config";
 
 /** @type {Config} */
 const config = {
 	...preset,
-	plugins: [...(preset.plugins ?? []), "prettier-plugin-tailwindcss"],
+	plugins: [
+		...(preset.plugins ?? []),
+		/**
+		 * The vscode prettier extension does not work with plugin imports.
+		 *
+		 * @see https://github.com/prettier/prettier-vscode/issues/3066
+		 */
+		/** @type {Plugin<PluginOptions>} */
+		"prettier-plugin-tailwindcss",
+	],
 	tailwindFunctions: ["cn", "styles"],
 	tailwindStylesheet: "./app/styles/index.css",
 };
