f3bb006b1087887de5dd1d05af050a316a10e040
stefanprobst <stefanprobst@posteo.de>
2025-09-12 12:17:51 +0200

    chore: update deps (#25)
    updates deps, pnpm, and some settings (notably disable eslint caching
    which does not play well with typescript-eslint - eslint works per file,
    but type-aware lint rules require whole-program analysis).

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,9 +1,9 @@
 import { join } from "node:path";
 
 import { defineConfig, devices } from "@playwright/test";
-import { isCI } from "ci-info";
 import { config as dotenv } from "dotenv";
 import { expand } from "dotenv-expand";
+import isCI from "is-in-ci";
 
 /**
  * Reading `.env` files here instead of using `dotenv-cli` so environment variables are
@@ -13,20 +13,21 @@ for (const envFilePath of [".env.test.local", ".env.local", ".env.test", ".env"]
 	expand(dotenv({ path: join(process.cwd(), envFilePath), quiet: true }));
 }
 
-const port = 3000;
+const port = Number(process.env.PORT) || 3000;
 const baseUrl = `http://localhost:${String(port)}`;
 
 export default defineConfig({
-	testDir: "./e2e",
-	snapshotDir: "./e2e/snapshots",
+	testDir: "../e2e",
+	snapshotDir: "../e2e/snapshots",
 	fullyParallel: true,
 	forbidOnly: isCI,
 	retries: isCI ? 2 : 0,
 	maxFailures: 10,
 	workers: isCI ? 1 : undefined,
-	reporter: isCI ? "github" : "html",
+	reporter: isCI ? [["github"], ["html", { open: "never" }]] : [["html"]],
 	use: {
 		baseURL: baseUrl,
+		screenshot: "on-first-failure",
 		trace: "on-first-retry",
 	},
 	projects: [
@@ -51,29 +52,29 @@ export default defineConfig({
 		},
 		/** Test against mobile viewports. */
 		// {
-		//      name: "Mobile Chrome",
-		//      use: { ...devices["Pixel 5"] },
-		//      dependencies: ["setup"],
+		// 	name: "Mobile Chrome",
+		// 	use: { ...devices["Pixel 5"] },
+		// 	dependencies: ["setup"],
 		// },
 		// {
-		//      name: "Mobile Safari",
-		//      use: { ...devices["iPhone 12"] },
-		//      dependencies: ["setup"],
+		// 	name: "Mobile Safari",
+		// 	use: { ...devices["iPhone 12"] },
+		// 	dependencies: ["setup"],
 		// },
 		/** Test against branded browsers. */
 		// {
-		//      name: "Microsoft Edge",
-		//      use: { ...devices["Desktop Edge"], channel: "msedge" },
-		//      dependencies: ["setup"],
+		// 	name: "Microsoft Edge",
+		// 	use: { ...devices["Desktop Edge"], channel: "msedge" },
+		// 	dependencies: ["setup"],
 		// },
 		// {
-		//      name: "Google Chrome",
-		//      use: { ...devices["Desktop Chrome"], channel: "chrome" },
-		//      dependencies: ["setup"],
+		// 	name: "Google Chrome",
+		// 	use: { ...devices["Desktop Chrome"], channel: "chrome" },
+		// 	dependencies: ["setup"],
 		// },
 	],
 	webServer: {
-		command: "pnpm run start",
+		command: `pnpm run start --port ${String(port)}`,
 		url: baseUrl,
 		reuseExistingServer: !isCI,
 	},
