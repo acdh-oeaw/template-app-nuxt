4370e16607828bda3137f09bab30fdd31c2db699
stefanprobst <stefanprobst@posteo.de>
2025-11-07 16:57:57 +0100

    fix: improve locale switcher messages (#35)
    this improves the locale switcher a11y by including the target language
    in its native spelling in the visually hidden message.

--- app/components/locale-switcher.vue
+++ app/components/locale-switcher.vue
@@ -1,32 +1,40 @@
 <script lang="ts" setup>
-import { locales } from "@/config/i18n.config";
+import { type Locale, locales } from "@/config/i18n.config";
 
 const currentLocale = useLocale();
 const t = useTranslations();
 const switchLocalePath = useSwitchLocalePath();
 const route = useRoute();
-const labels = computed(() => {
-	return new Intl.DisplayNames([currentLocale.value], { type: "language" });
+
+const names = computed(() => {
+	const names = {} as Record<Locale, Intl.DisplayNames>;
+
+	for (const locale of locales) {
+		names[locale] = new Intl.DisplayNames(locale, { type: "language" });
+	}
+
+	return names;
 });
 </script>
 
 <template>
 	<div class="flex items-center gap-2">
 		<template v-for="(locale, index) of locales" :key="locale">
-			<span v-if="index !== 0">|</span>
+			<span v-if="index !== 0" aria-hidden="true">|</span>
 
 			<NuxtLink
 				v-if="locale !== currentLocale"
 				:href="{ path: switchLocalePath(locale), query: route.query }"
 			>
 				<span class="sr-only">
-					{{ t("LocaleSwitcher.switch-locale", { locale: labels.of(locale) }) }}
+					{{ t("LocaleSwitcher.switch-locale", { locale: names[currentLocale].of(locale) }) }}
+					<span :lang="locale"> ({{ names[locale].of(locale) }})</span>
 				</span>
 				<span aria-hidden="true">{{ locale.toUpperCase() }}</span>
 			</NuxtLink>
 			<span v-else>
 				<span class="sr-only">
-					{{ t("LocaleSwitcher.current-locale", { locale: labels.of(locale) }) }}
+					{{ t("LocaleSwitcher.current-locale", { locale: names[locale].of(locale) }) }}
 				</span>
 				<span aria-hidden="true">{{ locale.toUpperCase() }}</span>
 			</span>
