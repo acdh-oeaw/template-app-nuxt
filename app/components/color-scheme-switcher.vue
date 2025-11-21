867b9b203b6b567c070b79b0130324263a37a23d
Stefan Probst <stefanprobst@posteo.de>
2025-11-21 14:27:20 +0100

    refactor: use nuxt/ui builtin color-scheme select

--- app/components/color-scheme-switcher.vue
+++ app/components/color-scheme-switcher.vue
@@ -1,20 +1,7 @@
 <script lang="ts" setup>
 const { t } = useI18n();
-
-const colorMode = useColorMode();
-
-const colorSchemes = ["system", "light", "dark"] as const;
 </script>
 
 <template>
-	<ClientOnly>
-		<label>
-			<span class="sr-only">{{ t("ColorSchemeToggle.change-color-scheme") }}</span>
-			<select v-model="colorMode.preference">
-				<option v-for="colorScheme of colorSchemes" :key="colorScheme" :value="colorScheme">
-					{{ t(`ColorSchemeToggle.color-schemes.${colorScheme}`) }}
-				</option>
-			</select>
-		</label>
-	</ClientOnly>
+	<UColorModeSelect :aria-label="t('ColorSchemeToggle.change-color-scheme')" />
 </template>
